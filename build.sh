b5d002da9e5d139951b27654f8317e1e0d99311b
AbdoAhmedElbanaa <115952879+AbdoAhmedElbanaa@users.noreply.github.com>
2025-09-22 12:07:49 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Remove old local_manifests
+rm -rf .repo/local_manifests/
+
+# ROM source repo
+repo init -u https://github.com/RisingOS-Revived/android -b sixteen-los --git-lfs
+
+# Clone repository
+git clone https://github.com/AbdoAhmedElbanaa/device_xiaomi_miatoll.git -b 16 device/xiaomi/miatoll
+git clone https://github.com/AbdoAhmedElbanaa/vendor_xiaomi_miatoll.git -b 16 vendor/xiaomi/miatoll
+git clone https://github.com/AbdoAhmedElbanaa/hardware_xiaomi.git -b 16 hardware/xiaomi
+git clone https://github.com/AbdoAhmedElbanaa/kernel_xiaomi_sm6250.git -b 16.0 kernel/xiaomi/sm6250
+
+# Sync
+/opt/crave/resync.sh
+
+# Set up build environment
+source build/envsetup.sh
+
+# Lunch
+riseup miatoll user
+
+# Build ROM
+rise b
