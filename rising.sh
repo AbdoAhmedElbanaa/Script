0fe6c942ec9a2da21c64b37652f2b5c930429e40
AbdoAhmedElbanaa <115952879+AbdoAhmedElbanaa@users.noreply.github.com>
2025-09-29 10:37:24 +0300

    Update rising.sh

--- rising.sh
+++ rising.sh
@@ -4,7 +4,7 @@
 rm -rf .repo/local_manifests/
 
 # ROM source repo
-repo init -u https://github.com/RisingOS-Revived/android -b sixteen-los --git-lfs
+repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
 
 # Clone repository
 git clone https://github.com/AbdoAhmedElbanaa/device_xiaomi_miatoll.git -b 16 device/xiaomi/miatoll
@@ -14,7 +14,7 @@ git clone https://github.com/AbdoAhmedElbanaa/kernel_xiaomi_sm6250.git -b 16.0 k
 git clone https://github.com/LineageOS/android_hardware_sony_timekeep.git -b lineage-22.2 hardware/sony/timekeep
 Git clone https://github.com/AbdoAhmedElbanaa/vendor_xiaomi_miuicamera.git -b 16 vendor/xiaomi/miuicamera
 # Updates
-git clone https://github.com/AbdoXXBuilder/android_packages_apps_Updater.git -b sixteen packages/apps/Updater
+git clone https://github.com/AbdoAhmedElbanaa/android_packages_apps_Updater.git -b sixteen packages/apps/Updater
 
 # Sync
 /opt/crave/resync.sh
